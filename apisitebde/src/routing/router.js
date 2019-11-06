b073ff26aadf231e0ae5fb0c95af0d172a72e438
basile <biblex1222@gmail.com>
2019-11-06 20:24:15 +0100

    add -Controller folder -userController.js[add register login and deleteUser in user Controller ] -routes for users in router

--- apisitebde/src/routing/router.js
+++ apisitebde/src/routing/router.js
@@ -1,9 +1,41 @@
 // Import
 const express = require('express');
+const userController = require('../Controllers/userController');
 
 // router declaration
 const router = express.Router();
 
-router.get('/test',(req, res) => {res.send('hello')});
+
+// ROUTES url : localhost:8080/api/#
+router.route('/test').get((req, res) => {res.send('hello')});
+
+
+//------------------------------- USER REQUESTS
+router.route('/user/register')
+    .post((req, res) => {
+        userController.register(req, res)
+    })
+    .get((req, res)=>{
+        userController.register(req, res)
+    });
+
+router.route('/user/login')
+    .post((req, res)=>{
+        userController.login(req, res)
+    })
+    .get((req, res)=>{
+        userController.login(req, res)
+    });
+
+router.route('/user/delete')
+    .post((req, res)=>{
+        userController.deleteUser(req, res)
+    })
+    .get((req, res)=>{
+        userController.deleteUser(req, res)
+    });
+//-------------------------------END USER REQUESTS
+
+
 
 module.exports = router;
