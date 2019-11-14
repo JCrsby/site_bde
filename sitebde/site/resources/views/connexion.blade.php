f0e70299f36bf13878a2f224f1a781b39ee8d538
Hyperyons <gaultier.geelen@viacesi.fr>
2019-11-14 10:30:27 +0100

    Connexion

--- sitebde/site/resources/views/connexion.blade.php
+++ sitebde/site/resources/views/connexion.blade.php
@@ -3,31 +3,38 @@
 
 @section('contenu')
 
-<body>
-<header>
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="#" ><img src="img/logobde.png"> </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" idp="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link border-left-0 border-right-0  border-bottom-1 border-top-0 active" href="#" >Accueil</a>
-            <a class="nav-item nav-link border-left-0 border-right-0  border-bottom-1 border-top-0" href="#">Boutique</a>
-        </div>
+    <body>
+    <div id="container">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h5 id="connection" align="center">Connexion</h5>
+        <form method="POST" action="" autocomplete="on" class="border mb-5 mt-5 mr-auto ml-auto col-4">
+            <div class="form-group mt-3">
+                <label for="email" class="uname" data-icon="u"> Adresse mail : </label>
+                <input id="email" name="email" class="form-control" required="required" type="text"
+                       placeholder="cesiLyon@viacesi.fr"/>
+            </div>
+            <div class="form-group">
+                <label for="password" class="youpasswd" data-icon="p"> Mot de passe : </label>
+                <input id="password" name="motDePasse" class="form-control" required="required" type="password"
+                       placeholder="CesiMDP123"/>
+            </div>
 
-        <div class="text-center">
-            <button type="submit" class="btn btn-outline-primary mt-2 text">Submit</button>
-        </div>
-        <p class="change_link text-center mb-0 mt-3">
-            <a href="#toregister" class="to_register mr-2"> Mot de passe oublié ?</a><a href="/inscription" class="to_register ml-2">Pas encore inscrit ?</a>
-        </p>
+            <div class="text-center">
+                <button type="submit" class="btn btn-outline-primary mt-2 text">Submit</button>
+            </div>
+            <p class="change_link text-center mb-0 mt-3">
+                <a href="#toregister" class="to_register mr-2"> Mot de passe oublié ?</a><a href="/inscription" class="to_register ml-2">Pas encore inscrit ?</a>
+            </p>
 
-    </form>
-</div>
-<br>
-<br>
-<br>
-<br>
+        </form>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
 
 @endsection
