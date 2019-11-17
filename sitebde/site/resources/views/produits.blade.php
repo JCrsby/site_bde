117bdd0f67207c0f598e557daee4553369b328fd
Hyperyons <gaultier.geelen@viacesi.fr>
2019-11-17 22:03:40 +0100

    Add add product

--- sitebde/site/resources/views/produits.blade.php
+++ sitebde/site/resources/views/produits.blade.php
@@ -7,102 +7,115 @@
     This page is reserved for the site administrator
     to be able to interact on events or products and even user
     -->
-<br>
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
+    <br>
 
-<div class="container" align="center">
-    <!-- title -->
-    <h4 class="text-uppercase" ><p>Administration</p></h4>
+    <div class="container" align="center">
+        <!-- title -->
+        <h4 class="text-uppercase"><p>Administration</p></h4>
 
-    <!-- option -->
-    <div class="card text-center">
-        <div class="card-header">
-            <ul class="nav nav-tabs card-header-tabs">
-                <li class="nav-item">
-                    <a class="nav-link" href="/utilisateur"><p class="text-dark">Utilisateur</p></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/evenements"><p class="text-dark">Evenements</p></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="/produits"><p class="text-dark">Produits</p></a>
-                </li>
-            </ul>
-        </div>
-        <!-- Action -->
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="userTable">
-                    <thead>
-                    <tr>
+        <!-- option -->
+        <div class="card text-center">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/utilisateur"><p class="text-dark">Utilisateur</p></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/evenements"><p class="text-dark">Evenements</p></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/produits"><p class="text-dark">Produits</p></a>
+                    </li>
 
-                        <th>ID</th>
-                        <th>Nom</th>
-                        <th>Bio</th>
-                        <th>Prix</th>
-                        <th>Image</th>
-                        <th>id Catégories</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
+                </ul>
             </div>
-            </tbody>
+            <!-- Action -->
+            <div class="card-body">
+                <input type='button' value='Ajouter produit' id='but_fetchall'>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="userTable">
+                        <thead>
+                        <tr>
 
-            </table>
+                            <th>ID</th>
+                            <th>Nom</th>
+                            <th>Bio</th>
+                            <th>Prix</th>
+                            <th>Image</th>
+                            <th>id Catégories</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-</div>
 
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
     <script type='text/javascript'>
-        $(document).ready(function(){
+        $(document).ready(function () {
+            $('#but_fetchall').click(function () {
+                $.ajax({
+                    url: 'http://localhost:3000/api/product/add',
+                    type: 'post',
+                    success: function (response) {
+                        var form = "<tr>" +
+                            "<td>" + "</td>" +
+                            "<td>" + "<input  name=\"name\" class=\"form-control\" required=\"required\" type=\"text\"\n" + "                       placeholder=\"cesiLyon@viacesi.fr\"/>" + "</td>" +
+                            "<td>" + "<input  name=\"description\" class=\"form-control\" required=\"required\" type=\"text\"\n" + "                       placeholder=\"cesiLyon@viacesi.fr\"/>" + "</td>" +
+                            "<td>" + "<input  name=\"price\" class=\"form-control\" required=\"required\" type=\"text\"\n" + "                       placeholder=\"cesiLyon@viacesi.fr\"/>" + "</td>" +
+                            "<td>" + "<input  name=\"img\" class=\"form-control\" required=\"required\" type=\"text\"\n" + "                       placeholder=\"cesiLyon@viacesi.fr\"/>" + "</td>" +
+                            "<td>" + "<select class=\"custom-select\" name=\"categories\" required>\n" +
+                            "                <option value=\"1\">Vêtements</option>\n" +
+                            "                <option value=\"2\">Goodies</option>" + "</select>" + "</td>" +
+                            "<td> " + "<button type=\"submit\" class=\"btn btn-outline-primary mt-2 text\">Envoyer</button>" + "<td>" +
+                            "</tr>" ;
+                        $("#userTable tbody").append(form);
+                            }});
 
+            });
             // Fetch all records
 
             fetchRecords(0);
 
-
-
-            fetchRecords(userid);
-
-
-
-
         });
 
-        function fetchRecords(id){
+        function fetchRecords(id) {
             $.ajax({
                 url: 'http://localhost:3000/api/product/admin',
                 type: 'post',
                 dataType: 'JSON',
-                success: function(response){
+                success: function (response) {
                     var string = JSON.parse(response);
                     console.log(string.value);
 
 
                     var len = 1;
                     $('#userTable tbody').empty(); // Empty <tbody>
-                    if(string.value != null){
+                    if (string.value != null) {
 
 
                     }
                     console.log(len);
 
 
-                    for(var i=0; i<len; i++){
+                    for (var i = 0; i < len; i++) {
                         var id = string.value[i].id_PRODUIT;
                         var name = string.value[i].Nom;
                         var description = string.value[i].Description;
@@ -121,7 +134,8 @@
 
                         $("#userTable tbody").append(tr_str);
 
-                    } if(response['data'] != null){
+                    }
+                    if (response['data'] != null) {
                         var tr_str = "<tr>" +
                             "<td align='center'>1</td>" +
                             "<td align='center'>" + response['data'].username + "</td>" +
